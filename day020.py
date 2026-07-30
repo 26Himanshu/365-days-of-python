637668db7224655ebcc0db03925fa0cea878968d
Himanshu <himanshu.jan26@gmail.com>
2026-07-30 21:59:49 +0530

    done two projects

---
+++ day020.py
@@ -0,0 +1,48 @@
+# Random no guessing game
+
+# import random 
+
+# com=random.randint(1,100)
+# trie=0
+
+# while True:
+#     trie = trie + 1
+#     hum=int(input("guess your no between 1 to 100"))
+
+#     if hum==com:
+#             print (f"you won the game in {trie}")
+#             break
+#     elif hum>com:
+#             print("sorry wrong guess go lower")
+#     elif hum<com:
+#             print("sorry wrong guess go higher")
+
+
+
+#
+# import random 
+# com=random.randint(1,100)
+# tries=0
+
+
+# while True:
+#     hum=int(input("guess your no between 1 to 100"))
+#     count+=1
+#     if hum==com:
+#         print (f"you won the game in {tries} count")
+#         break
+#     elif hum>com:
+#         print('sorry wroong guess go lower')
+#     elif hum<com:
+#         print("sorry wrong guess go higher")
+        
+
+#creating file management system CRUD operations
+#create file , read file , update file , delete file
+
+print("press 1 for creating a file")
+print("press 2 for reading a file")
+print("press 3 for updating a file")
+print("press 4 for deleting a file")
+
+a=int(input("\n enter your choice:-"))
