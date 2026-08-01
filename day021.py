a49b0e5057e7ca7374dfab65a9b08016afb16d61
Himanshu <himanshu.jan26@gmail.com>
2026-08-01 21:37:13 +0530

    some questions

---
+++ day021.py
@@ -0,0 +1,22 @@
+#code with harry 
+print("hello himanshu this side \n I am learning python from code with harry")
+
+
+print("hello himanshu this side'and i am himanshu'")
+
+#random no guessing game
+
+import random
+com=random.randint(1,100)
+tries=0
+
+while True:
+    tries+=1
+    hum=int(input("enter the no between 1 to 100:-"))
+    if com==hum:
+        print(f"you won the game in {tries} tries")
+        break
+    elif com>hum:
+        print("sorry wrong guess go higher")
+    elif com<hum:
+        print("sorry wrong guess go lower")
