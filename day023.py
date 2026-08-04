0b7186002caac37086728e883d68c189d240a5d3
Himanshu <himanshu.jan26@gmail.com>
2026-08-04 20:46:33 +0530

    nothing done

--- day023.py
+++ day023.py
@@ -1,6 +1,39 @@
 #print table of no 
 
-n=int(input("enter the no"))
-for i in range(n , n*10+1 , n):
-    print(i)
+# n=int(input("enter the no"))
+# for i in range(n , n*10+1 , n):
+#     print(i)
     
+
+#factorial of a no 
+
+# n=int(input("enter the no"))
+# fact=1
+# for i in range(1,n+1):
+#     fact=fact*i
+# print(f'the factorial of the no is {fact}')
+
+
+
+#to find the sum of n natural no
+
+n=int(input("enter the no"))
+sum=0
+for i in range(1,n+1):
+    sum+=i
+print(f'the sum of n natural no is {sum}')
+
+
+
+#to find the sum of n natural no 
+n=int(input("enter the no"))
+sum=0
+for i in range(1,n+1):
+    sum=sum+i
+print(f'the sum of n natural no is {sum}')
+
+
+#to find the square of a no
+n=int(input("enter the no"))
+for i in n:
+    print(f'the square of the no is {n**2}')
