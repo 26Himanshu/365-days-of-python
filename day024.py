ca71d0d0692c8caf2fa4ad11c02687ba3ef478c3
Himanshu <himanshu.jan26@gmail.com>
2026-08-05 21:46:11 +0530

    revision

---
+++ day024.py
@@ -0,0 +1,29 @@
+# a=int(input("enter the no"))
+# sum=0
+# for i in range(1,a+1):
+#     sum+=i
+# print(f"the sum of n natural no is {sum}")
+
+
+# n=int(input("enter the no:-"))
+# fac=1
+# for i in range(1,n+1):
+#     fac=fac*i
+# print(f"the factorial of the no is {fac}")
+
+
+a={"apple":1,}
+b={"banana":2}
+a.update(b)
+print(a)
+
+
+a=[34322,54,24362,6,7,3,724727]
+a.append(83612356835618651896581265)
+print(a)
+
+a.pop(2)
+print(a)
+
+a.clear()
+print(a)
